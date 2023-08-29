7590f8bf59506c99c7c49e5f4f37f3ee0cd9e1f4
Márcio Xavier <43189549+xaviermarcio@users.noreply.github.com>
2023-08-29 18:51:39 -0300

    Finalização do projeto Qr Code

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ const imgQrCode = document.querySelector('#imgQrCode');
 const wrapper = document.querySelector('.wrapper');
 let valueDefault;
 
-// https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example 
+
 
 btnValue.addEventListener('click', () => {
     let qrcodeValue = inputValue.value.trim();  
     if(!qrcodeValue || qrcodeValue === valueDefault) return;
     valueDefault = qrcodeValue;
     btnValue.innerText = 'Gerando QR Code...'
-    imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${valueDefault}`
+    imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${valueDefault}` /*https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example*/
     imgQrCode.addEventListener('load', () => {
         wrapper.classList.add('active');
         btnValue.innerText = 'Gerar QRCode'
